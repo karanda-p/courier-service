92d761bdad79f5fd6a47904ec5b9f54dddd313ca
Pavel Karanda <77533722+karanda-p@users.noreply.github.com>
2021-05-12 15:24:38 +0300

    Rabbitmq update

--- src/main/java/ru/itfbgroup/courierservice/service/ListenerService.java
+++ src/main/java/ru/itfbgroup/courierservice/service/ListenerService.java
@@ -1,15 +1,29 @@
 package ru.itfbgroup.courierservice.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import ru.itfbgroup.courierservice.model.Order;
+import ru.itfbgroup.courierservice.model.Status;
+import ru.itfbgroup.courierservice.model.Task;
+import ru.itfbgroup.courierservice.repository.TaskRepository;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ListenerService {
 
+    private final TaskRepository taskRepository;
+    private final TaskService taskService;
+
     @RabbitListener(queues = "${message.courier-queue}")
-    public void receive(){
+    public void receive(Order order){
+        Task task = new Task();
+        task.setOrderId(order.getId());
+        task.setAddress(order.getAddress());
+        task.setStatus(Status.ACCEPTED);
+        taskService.sendTask(taskRepository.save(task));
         log.info("!!!");
     }
 }
--- src/main/java/ru/itfbgroup/courierservice/service/TaskService.java
+++ src/main/java/ru/itfbgroup/courierservice/service/TaskService.java
@@ -1,6 +1,9 @@
 package ru.itfbgroup.courierservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.itfbgroup.courierservice.model.Order;
@@ -13,6 +16,11 @@ import ru.itfbgroup.courierservice.repository.TaskRepository;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final RabbitTemplate rabbitTemplate;
+    private final TopicExchange exchange;
+
+    @Value("${message.food-delivery-routing-key}")
+    private String routingKey;
 
     @Transactional
     public Task createNewTask(Order order){
@@ -22,5 +30,9 @@ public class TaskService {
         task.setStatus(Status.ACCEPTED);
         Task savedTask = taskRepository.save(task);
         return savedTask;
+    }
+
+    public void sendTask(Task task){
+        rabbitTemplate.convertAndSend(exchange.getName(), routingKey, task);
     }
 }
